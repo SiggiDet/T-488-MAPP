0f583eea2d8a9d6c96eda1a2fc8a205f93cec14e
Sigurður Detlef <siggidetjons6@gmail.com>
2021-11-22 14:36:13 +0000

    Adding everything to check whether everything appears now

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from  'react-native';
+
+const HelloWorld = () => {
+    return (
+        <view>
+            <Text>Hello World g</Text>
+        </view>
+    )
+}
+
+export default HelloWorld;
